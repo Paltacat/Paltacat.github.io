29c366a2e00af2beb4b1e79cb155915fda6a1a39
nicolasgabrielli <nicolas.gabrielli@usach.cl>
2023-10-03 10:49:09 -0300

    Fix reel + animaciones

--- js/script.js
+++ js/script.js
@@ -7,14 +7,19 @@ window.onscroll = () => {
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         if (top >= offset && top < offset + height) {
+            if (sec.classList.contains('home')) {
+                sec.parentElement.setAttribute('transition-style', 'in:wipe:right');
+            }
             sec.classList.add('show-animate');
         }
         else {
+            if (sec.classList.contains('home')) {
+                sec.parentElement.setAttribute('transition-style', '');
+            }
             sec.classList.remove('show-animate');
         }
     })
 }
-
 
 const currentPage = 1;
 const gallery = ['img1.jpg', 'img2.jpg', 'img3.jpg', 'img4.jpg', 'img5.jpg', 'img6.jpg', 'img7.png', 'img8.png',
@@ -95,38 +100,3 @@ function displayAnimations(pageNumber) {
     document.getElementById('animations-page-' + pageNumber).classList.add('active');
 }
 displayAnimations(1);
-
-
-const vimeoIds = ["868951760", "868951760", "868951760", "868951760", "868951760", "868951760", "868951760", "868951760", "868951760"];
-function displayVideos(pageNumber) {
-    const currentGallery = [];
-    document.getElementById('videos-row-1').innerHTML = '';
-    document.getElementById('videos-row-2').innerHTML = '';
-    for (let i = 1; i <= Math.ceil(vimeoIds.length / 8); i++) {
-        document.getElementById('videos-page-' + i).classList.remove('active');
-    }
-
-    for (let i = ((pageNumber - 1) * 2); i < ((pageNumber - 1) * 2) + 2; i++) {
-        if (i >= vimeoIds.length) break;
-        currentGallery.push(vimeoIds[i]);
-    }
-
-    for (let i = 0; i < currentGallery.length; i++) {
-        if (i >= vimeoIds.length) break;
-        const div = document.createElement('div');
-        div.classList.add('images');
-        div.classList.add('animate');
-        const iframe = document.createElement('iframe');
-        iframe.src = "https://player.vimeo.com/video/" + currentGallery[i] + "?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479";
-        iframe.setAttribute("frameborder", "0");
-        iframe.setAttribute("allow", "autoplay; fullscreen; picture-in-picture");
-        div.appendChild(iframe);
-        if (i >= currentGallery.length / 2) {
-            document.getElementById('videos-row-2').appendChild(div);
-        } else {
-            document.getElementById('videos-row-1').appendChild(div);
-        }
-    }
-    document.getElementById('videos-page-' + pageNumber).classList.add('active');
-}
-displayVideos(1);
